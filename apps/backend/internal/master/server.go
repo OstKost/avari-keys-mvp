d51e0bd4f0f21d68d6cd86d8963c6056c2631241
OstKost <web@ostkost.ru>
2026-09-25 11:18:38 +0300

    chore(release): bump version to v0.2.1

--- apps/backend/internal/master/server.go
+++ apps/backend/internal/master/server.go
@@ -239,7 +239,7 @@ func (s *Server) handleHealth(w http.ResponseWriter, r *http.Request) {
 	s.writeJSON(w, http.StatusOK, models.HealthResponse{
 		Status:  "ok",
 		Service: "avari-master",
-		Version: "v0.2.0",
+		Version: "v0.2.1",
 	})
 }
 
